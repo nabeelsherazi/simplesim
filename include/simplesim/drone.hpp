01ea9b3f487354f7136ab7688797ea3e4acfd3eb
Nabeel Sherazi <nabeelsherazi97@gmail.com>
2024-10-01 15:55:38 -0700

    begin migration to tf2

--- include/simplesim/drone.hpp
+++ include/simplesim/drone.hpp
@@ -1,8 +1,13 @@
 #pragma once
 
+#include "simplesim/pch.hpp"
+
+#include <tf2_ros/transform_broadcaster.h>
 #include <SFML/Graphics.hpp>
 #include <filesystem>
+#include <geometry_msgs/msg/transform_stamped.hpp>
 #include <geometry_msgs/msg/vector3.hpp>
+#include <nav_msgs/msg/odometry.hpp>
 #include <random>
 #include <rclcpp/rclcpp.hpp>
 
@@ -17,37 +22,69 @@ struct DroneOptions {
     /// or acceleration command)
     enum class ControlMode { Velocity, Acceleration };
 
+    /// @brief Simulated control mode (whether to accept a velocity
+    /// or acceleration command)
     DroneOptions::ControlMode controlMode = DroneOptions::ControlMode::Acceleration;
+
+    /// @brief Initial position for the drone
     sf::Vector2f initialPosition = {0.0f, 0.0f};
+
+    /// @brief Magnitude of the maximum "jerk" to the wind at any iteration. Current wind speed is calculated like
+    /// currentWind += rand(-intensity, intensity)
     float windIntensity = 0.0f;
+
+    /// @brief Maximum acceleration command the drone will accept
+    float maxAcceleration = 10.0f;
+
+    /// @brief Drag constant in the linear regime (friction drag)
+    float linearDragConstant = 0.0005f;
+
+    /// @brief Drag constant in the quadratic regime (pressure drag)
+    float quadraticDragConstant = 0.0005f;
+
+    /// @brief Speed threshold at which to switch from linear to quadratic drag
+    float quadraticDragThreshold = 500.0f;
 };
 
+/// @brief Simulates vehicle dynamics given control and environment forces
 class Drone : public rclcpp::Node, public Renderable {
    public:
-    /// @brief Class that represents the "real world" drone
-    /// @param node_name Name of the ROS2 node
-    /// @param controlMode the control mode offered
+    /// @brief Simulates vehicle dynamics given control and environment forces
+    /// @param node_name The name of the node to launch under
+    /// @param options Simulation options struct
     Drone(std::string node_name, DroneOptions& options);
 
+    /// @brief Destructor
     virtual ~Drone();
 
+    /// @brief Takes a sprite by move to use as the visual representation of this drone
+    /// @param sprite sprite instance to use
     void setSprite(ManagedSprite&& sprite);
 
+    /// @brief Callback that sets current commanded acceleration (acceleration control mode)
+    /// @param msg message
     void setAccelerationCommand(const geometry_msgs::msg::Vector3::SharedPtr msg);
 
+    /// @brief Callback that sets current commanded velocity (velocity control mode)
+    /// @param msg message
     void setVelocityCommand(const geometry_msgs::msg::Vector3::SharedPtr msg);
 
     /// @brief Advance the simulation
     /// @param dt the amount of time that has passed since the last call
     void tick(const sf::Time dt);
 
+    /// @brief Publish simulation state
     void publish();
 
+    /// @brief Reset the simulation state to initial conditions
     void reset();
 
     std::vector<const sf::Drawable*> getDrawables() const override;
 
     sf::Vector2f currentWind;
+    sf::Vector2f currentDrag;
+
+    DroneOptions options;
 
    private:
     ManagedSprite sprite;
@@ -62,7 +99,11 @@ class Drone : public rclcpp::Node, public Renderable {
     std::mt19937 randomGenerator;
     std::uniform_real_distribution<> dist;
 
+    rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odometryPublisher;
+    std::shared_ptr<tf2_ros::TransformBroadcaster> transformBroadcaster;
+
     rclcpp::Publisher<geometry_msgs::msg::Vector3>::SharedPtr position_publisher;
+    rclcpp::Publisher<geometry_msgs::msg::Vector3>::SharedPtr velocity_publisher;
     rclcpp::Subscription<geometry_msgs::msg::Vector3>::SharedPtr accelerationCommandSubscriber;
     rclcpp::Subscription<geometry_msgs::msg::Vector3>::SharedPtr velocityCommandSubscriber;
 };
--- src/drone.cpp
+++ src/drone.cpp
@@ -1,30 +1,49 @@
 #include "simplesim/drone.hpp"
+
+#include "simplesim/pch.hpp"
+
 #include <filesystem>
-#include <geometry_msgs/msg/vector3.hpp>
+#include <memory>
 #include <random>
 #include <utility>
+#include <vector>
+
+#include <SFML/Graphics.hpp>
+#include <geometry_msgs/msg/vector3.hpp>
+#include <nav_msgs/msg/odometry.hpp>
+#include <rclcpp/rclcpp.hpp>
+
+#include "simplesim/utils.hpp"
 
 Drone::Drone(std::string node_name, DroneOptions& options) : Node(node_name) {
+    // Options
+    this->options = options;
     // Initial position
     this->currentPosition = options.initialPosition;
     this->position_publisher = this->create_publisher<geometry_msgs::msg::Vector3>("/simplesim/drone/position", 10);
+    this->velocity_publisher = this->create_publisher<geometry_msgs::msg::Vector3>("/simplesim/drone/velocity", 10);
+    this->odometryPublisher = this->create_publisher<nav_msgs::msg::Odometry>("/simplesim/drone/odometry", 10);
+    this->transformBroadcaster = std::make_shared<tf2_ros::TransformBroadcaster>(this);
     // Control mode
+    this->controlMode = options.controlMode;
     switch (options.controlMode) {
         case DroneOptions::ControlMode::Acceleration:
             this->accelerationCommandSubscriber = this->create_subscription<geometry_msgs::msg::Vector3>(
-                "/simplesim/drone/accel_cmd", 10, std::bind(&Drone::setAccelerationCommand, this, _1));
+                "/simplesim/drone/accel_cmd", 10,
+                [=](const std::shared_ptr<geometry_msgs::msg::Vector3> msg) { this->setAccelerationCommand(msg); });
             break;
         case DroneOptions::ControlMode::Velocity:
             this->velocityCommandSubscriber = this->create_subscription<geometry_msgs::msg::Vector3>(
-                "/simplesim/drone/velocity_cmd", 10, std::bind(&Drone::setVelocityCommand, this, _1));
+                "/simplesim/drone/velocity_cmd", 10,
+                [=](const std::shared_ptr<geometry_msgs::msg::Vector3> msg) { this->setVelocityCommand(msg); });
             break;
         default:
             RCLCPP_ERROR(this->get_logger(), "Unknown control mode requested");
             throw std::runtime_error("Unknown control mode requested");
     }
     // Noise generator
-    randomGenerator = std::mt19937(randomDevice());
-    dist = std::uniform_real_distribution<>(-options.windIntensity, options.windIntensity);
+    this->randomGenerator = std::mt19937(randomDevice());
+    this->dist = std::uniform_real_distribution<>(-options.windIntensity, options.windIntensity);
 };
 
 Drone::~Drone() {};
@@ -49,7 +68,20 @@ void Drone::tick(const sf::Time dt) {
     // Update current wind speed as a random walk
     currentWind.x += dist(randomGenerator);
     currentWind.y += dist(randomGenerator);
-    this->currentVelocity += dt.asSeconds() * this->currentWind;
+
+    // Update current drag
+    // Smoothly blend between linear and quadratic drag up to the quadratic threshold, then just use quadratic drag
+    auto velocitySquared = simplesim::squareComponents(this->currentVelocity);
+    auto linearDrag = -this->options.linearDragConstant * this->currentVelocity;
+    auto quadraticDrag = -this->options.quadraticDragConstant * velocitySquared;
+    if (simplesim::norm(this->currentVelocity) < this->options.quadraticDragThreshold) {
+        this->currentDrag = linearDrag;
+    } else {
+        this->currentDrag = quadraticDrag;
+    }
+
+    // Integrate for new velocity
+    this->currentVelocity += dt.asSeconds() * (this->currentWind + this->currentDrag);
 
     // In acceleration control mode, integrate for new velocity (otherwise receive new velocity directly)
     if (this->controlMode == DroneOptions::ControlMode::Acceleration) {
@@ -63,10 +95,39 @@ void Drone::tick(const sf::Time dt) {
 }
 
 void Drone::publish() {
-    geometry_msgs::msg::Vector3 msg;
-    msg.x = this->currentPosition.x;
-    msg.y = this->currentPosition.y;
-    this->position_publisher->publish(msg);
+    geometry_msgs::msg::Vector3 position_msg;
+    position_msg.x = this->currentPosition.x;
+    position_msg.y = this->currentPosition.y;
+    geometry_msgs::msg::Vector3 velocity_msg;
+    velocity_msg.x = this->currentVelocity.x;
+    velocity_msg.y = this->currentVelocity.y;
+    this->position_publisher->publish(position_msg);
+    this->velocity_publisher->publish(velocity_msg);
+
+    // Publish odometry message
+    nav_msgs::msg::Odometry odom_msg;
+    odom_msg.header.stamp = this->now();
+    odom_msg.header.frame_id = "odom";
+    odom_msg.child_frame_id = "base_link";
+    odom_msg.pose.pose.position.x = this->currentPosition.x;
+    odom_msg.pose.pose.position.y = this->currentPosition.y;
+    odom_msg.twist.twist.linear.x = this->currentVelocity.x;
+    odom_msg.twist.twist.linear.y = this->currentVelocity.y;
+    this->odometryPublisher->publish(odom_msg);
+
+    // Publish transform
+    geometry_msgs::msg::TransformStamped transformStamped;
+    transformStamped.header.stamp = this->now();
+    transformStamped.header.frame_id = "odom";
+    transformStamped.child_frame_id = "base_link";
+    transformStamped.transform.translation.x = this->currentPosition.x;
+    transformStamped.transform.translation.y = this->currentPosition.y;
+    transformStamped.transform.translation.z = 0.0;
+    transformStamped.transform.rotation.w = 1.0;
+    transformStamped.transform.rotation.x = 0.0;
+    transformStamped.transform.rotation.y = 0.0;
+    transformStamped.transform.rotation.z = 0.0;
+    transformBroadcaster->sendTransform(transformStamped);
 }
 
 void Drone::reset() {
